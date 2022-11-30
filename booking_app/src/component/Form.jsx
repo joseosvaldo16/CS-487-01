aa60247230205ce9d19462176838dce9c9726c1d
Jose Vera <jvera3@hawk.iit.edu>
2022-11-30 02:06:00 -0600

    Fixed date constraints Changed constraints of start date and end date selection options, so that end date is greater than start date, and start days is >= to today.

--- booking_app/src/component/Form.jsx
+++ booking_app/src/component/Form.jsx
@@ -1,7 +1,14 @@
 import React, { useState, setState } from 'react'
+
 import './Form.css'
 
-export default function Form({handleClick}) {
+export const toDateInputConstraint = (date) => (date || new Date()).toISOString().slice(0, 10)
+
+export const Form = ({ handleClick }) => {
+
+
+  const minDate = (new Date()).toISOString().slice(0, 10)
+
   const [data, setData] = useState({
     name: "",
     room: "Library",
@@ -12,28 +19,49 @@ export default function Form({handleClick}) {
   })
 
   function changeName(e) {
-    setData({...data, name: e.target.value})
+    setData({ ...data, name: e.target.value })
   }
 
   function changeRoom(e) {
-    setData({...data, room: e.target.value})
+    setData({ ...data, room: e.target.value })
   }
 
   function changeStartTime(e) {
-    setData({...data, startTime: e.target.value})
+    setData({ ...data, startTime: e.target.value })
   }
 
   function changeEndTime(e) {
-    setData({...data, endTime: e.target.value})
+    setData({ ...data, endTime: e.target.value})
   }
 
   function changeStartDate(e) {
-    setData({...data, startDate: e.target.value})
+    const newDate = e.target.valueAsDate
+    let newEndDate = data.endDate
+
+    if (newDate.getTime() > newEndDate.getTime()){
+
+        newEndDate = newDate
+    } 
+
+    setData({ ...data, startDate: e.target.valueAsDate, endDate: newEndDate})
+  }
+
+  function changeStartDate(e) {
+    const newDate = e.target.valueAsDate
+    setData({
+      ...data,
+      startDate: e.target.valueAsDate,
+      endDate: (newDate.getTime() > data.endDate.getTime()) ?
+        newDate : data.endDate,
+    })
   }
 
   function changeEndDate(e) {
-    setData({...data, endDate: e.target.value})
+    setData({ ...data, endDate: e.target.valueAsDate})
   }
+
+
+
 
   function clear() {
     setData({
@@ -46,8 +74,6 @@ export default function Form({handleClick}) {
     })
   }
 
-  
-
   return (
     <div className = 'content'>
         
@@ -59,11 +85,23 @@ export default function Form({handleClick}) {
                         </select></h2>
                         </div>
         <div>  
-          <h2>Start Date: <input className='input' height = {50} onChange={e=>changeStartDate(e)} type="date"></input></h2>
+          <h2>Start Date: <input 
+          className='input' 
+          height = {50} 
+          onChange={e=>changeStartDate(e)} 
+          type="date"
+          min={toDateInputConstraint()}
+          value={toDateInputConstraint(data.startDate)}
+          ></input></h2>
         </div>
         <h2>Start Time: <input className='input' onChange={e=>changeStartTime(e)} type="time"></input></h2>
         <div>
-          <h2>End Date: <input className='input' onChange={e=>changeEndDate(e)} type="date"></input></h2>
+          <h2>End Date: <input className='input' 
+          onChange={e=>changeEndDate(e)} 
+          type="date"
+          min={toDateInputConstraint(data.startDate)}
+          value={toDateInputConstraint(data.endDate)}
+          ></input></h2>
         </div>
         <h2>End Time: <input className='input' onChange={e=>changeEndTime(e)} type="time"></input></h2>
         <div className='button-container'>
@@ -74,5 +112,12 @@ export default function Form({handleClick}) {
         
     </div>
   )
+
+
+
+
+
 }
 
+
+export default Form
